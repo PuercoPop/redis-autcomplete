68ca92ad12e268965e3715c5bfe11eb3b7bc2daa
Javier Olaechea <javier@able.co>
2018-06-08 18:35:05 -0500

    First pairing session with Gianfranco
    - Add two commands to the REPL: ADD_CANDIDATE
      GET_CANDIDATES. ADD_CANDIDATE doesn't work, we couldn't try if
      GET_CANDIDATES works either.

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 CC = gcc
-CFLAGS = -std=c99 -Wall -Wextra -Werror -pedantic -D_GNU_SOURCE
+CFLAGS = -std=c99 -Wall -Wextra  -pedantic -g -D_GNU_SOURCE
 INCS=-I/usr/include -I.
 LIBS = -L/usr/lib -lc
 
 
-repl: repl.c
-	@$(CC) $(CFLAGS) repl.c -o repl $(INCS) $(LIBS) -lreadline
+repl: repl.c map.c
+	@$(CC) $(CFLAGS) repl.c map.c -o repl $(INCS) $(LIBS) -lreadline
--- repl.c
+++ repl.c
@@ -1,14 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdbool.h>
 #include <string.h>
 #include <signal.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 
+#include "map.h"
+
 #define UNUSED __attribute__((unused))
 
+enum command_t {
+  ADD_CANDIDATE,
+  GET_CANDIDATES,
+  UNKNOWN_COMMAND,
+};
+
+typedef struct Command {
+  enum command_t type;
+  char *candidate;
+} Command;
+
 static struct sigaction sa;
+
+static struct Map *candidates_map;
 
 static char *input;
 static char *prompt = "db> ";
@@ -17,7 +33,9 @@ static char *history_file;
 void sigint(const int);
 void setup();
 void die(char *, ...);
-
+Command parse_command(char *);
+void execute_command(Command);
+bool print_candidate(const char *, void *, void *);
 
 void die(char *msg, ...)
 {
@@ -36,21 +54,74 @@ sigint(const int UNUSED signo)
         exit(EXIT_SUCCESS);
 }
 
+bool print_candidate(const char *key, void *value, UNUSED void *date)
+{
+  printf("Candidate: %s | %s\n", key, (char *)value);
+  return true;
+}
+
+Command
+parse_command(char *input)
+{
+  Command ret;
+
+  // TODO: Replace with strsep
+  char *keyword_add_candidate = strtok(input, " ");
+  char *candidate = strtok(NULL, " ");
+
+  if (!strncasecmp("ADD_CANDIDATE", keyword_add_candidate, 13) && candidate) {
+    ret.type = ADD_CANDIDATE;
+    ret.candidate = input;
+  } else if (!strncasecmp("GET_CANDIDATES", keyword_add_candidate, 14)) {
+      ret.type = GET_CANDIDATES;
+  } else {
+    ret.type = UNKNOWN_COMMAND;
+  }
+  return ret;
+}
+
+void
+execute_command(Command cmd)
+{
+  bool err;
+  switch(cmd.type) {
+  case ADD_CANDIDATE:
+    err = map_put(candidates_map, cmd.candidate, NULL);
+    if(err) {
+      printf("Candidate %s added.\n", cmd.candidate);
+    } else {
+      printf("Unable to add Candidate %s.\n", cmd.candidate);
+    }
+    break;
+  case GET_CANDIDATES:
+    map_iterate(candidates_map, &print_candidate, NULL);
+    break;
+  default:
+    printf("Unknown command\n");
+  }
+}
+
 void
 setup()
 {
+  	
         int err;
 
         memset(&sa, 0, sizeof(sa));
         sa.sa_handler = sigint;
         if(sigaction(SIGINT, &sa, NULL))
                 die("Could not install SIGINT handler.\n");
+
+        
+        candidates_map = map_new();
         
+        
         const char *home = getenv("HOME");
         if(!home || !*home)
                 die("$HOME is not set.\n");
 
         err = asprintf(&history_file, "%s/.acr_repl_history", home);
+
         if (err <0)
                 die("Could not allocate readline history file name.\n");
 
@@ -62,6 +133,7 @@ int
 main(UNUSED int argc, UNUSED char *argv[])
 {
         setup();
+        Command cmd;
         for(;;) {
                 input = readline(prompt);
                 if(!input) {
@@ -69,12 +141,11 @@ main(UNUSED int argc, UNUSED char *argv[])
                         write_history(history_file);
                         exit(EXIT_SUCCESS);
                 }
+                add_history(input);
+                cmd = parse_command(input);
+                execute_command(cmd);
         }
-        add_history(input);
-        write_history(input);
-
-        // IOU
-
+        
         free(input);
 
         return EXIT_SUCCESS;
